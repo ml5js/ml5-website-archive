16c558d69ad1eaba96d0b7af4364ef0d230808a2
wenqili <wenqi.li@nyu.edu>
2019-05-12 02:02:35 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,25 @@ import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section className="ml5Grid__wrapper">
+      <div className="ml5Grid__container">
+        <section className="ml5Grid__sidebar">
+          <div className="Sidebar__container" />
+        </section>
+
+        <div className="ml5Grid__content">
+          <h1>Sorry, we lost a page here, but we found a Shiffman video!</h1>
+          <div class="iframe__container iframe__container--video">
+            <iframe
+              src="https://www.youtube.com/embed/jmznx0Q1fP0"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
