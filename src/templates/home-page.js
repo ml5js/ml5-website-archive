1c9422db3808d6bc8f9344e42ebc8da675a85979
joeyklee <joeyklee@nyu.edu>
2020-06-02 17:53:12 -0400

    adds solidarity statement modal

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
@@ -6,6 +6,92 @@ import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 import Layout from "../components/Layout";
 import Features from "../components/Features";
 import Features2 from "../components/Features2";
+
+const BLMSolidarityModal = () => {
+  const [display, setDisplay] = useState({ display: true });
+
+  const handleClose = (evt) => {
+    setDisplay({ display: false });
+  };
+
+  return (
+    <div
+      class="modal-container"
+      style={{
+        display: display.display === true ? "flex" : "none",
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100vh",
+        zIndex: 9999999,
+        backgroundColor: "rgba(0,0,0,0.85)",
+      }}
+    >
+      <div class="modal" style={{
+          maxWidth:"800px",
+          padding:"1rem",
+          borderRadius: "4px",
+          backgroundColor: "black",
+          color:"white",
+          lineHeight: 1.4,
+          margin:"0 auto",
+          border:"2px solid yellow",
+        }}>
+        <header class="modal-header">
+          <h1 style={{maxWidth:"600px", color:"#A15FFB"}}>
+            The ml5.js community stands in solidarity with Black Lives Matter.
+          </h1>
+        </header>
+        <main class="modal-main" style={{maxHeight:"200px",
+          overflowY:"scroll", marginBottom:"1rem"}}>
+          <p style={{marginBottom:"1rem"}}>
+            George Floyd, Natosha McDade, Yassin Mohamed, Finan H. Berhe, Sean
+            Reed, Steven Demarco Taylor, Breonna Taylor, Ariane McCree, Terrance
+            Franklin, Miles Hall, Darius Tarver, William Green, Samuel David
+            Mallard, Kwame Jones, De’von Bailey, Christopher Whitfield, Anthony
+            Hill, De’Von Bailey, Eric Logan, Jamarion Robinson, Gregory Hill Jr,
+            JaQuavion Slaton, Ryan Twyman, Brandon Webber, Jimmy Atchison,
+            Willie McCoy, Emantic Fitzgerald Bradford J, D’ettrick Griffin,
+            Jemel Roberson, DeAndre Ballard, Botham Shem Jean, Robert Lawrence
+            White, Anthony Lamar Smith, Ramarley Graham, Manuel Loggins Jr,
+            Trayvon Martin, Wendell Allen, Kendrec McDade, Larry Jackson Jr,
+            Jonathan Ferrell, Jordan Baker, Victor White III, Dontre Hamilton,
+            Eric Garner, John Crawford III, Michael Brown, Ezell Ford, Dante
+            Parker, Kajieme Powell, Laquan McDonald, Akai Gurley, Tamir Rice,
+            Rumain Brisbon, Jerame Reid, Charly Keunang, Tony Robinson, Walter
+            Scott, Freddie Gray, Brendon Glenn, Samuel DuBose, Christian Taylor,
+            Jamar Clark, Mario Woods, Quintonio LeGrier, Gregory Gunn, Akiel
+            Denkins, Alton Sterling, Philando Castile, Terrence Sterling,
+            Terence Crutcher, Keith Lamont Scott, Alfred Olango, Jordan Edwards,
+            Stephon Clark, Danny Ray Thomas, DeJuan Guillory, Patrick Harmon,
+            Jonathan Hart, Maurice Granton, Julius Johnson, Jamee Johnson,
+            Michael Dean, and too many more to list here...
+          </p>
+        </main>
+        <p style={{marginBottom:"1rem"}}>
+            Please consider donating to <a href="https://blacklivesmatter.com/" noreferrer>Black Lives Matter</a>, 
+            <a href="https://www.naacpldf.org/" noreferrer>The NAACP Legal Defense and Educational Fund</a>, 
+            <a href="https://eji.org/">The Equal Justice Initiative</a>, 
+            <a href="https://www.wetheprotesters.org/">We The Protesters and Campaign Zero</a>,
+            <a href="https://codecooperative.org/">Code Cooperative</a>,
+             and 
+            <a href="https://www.gofundme.com/f/georgefloyd"> George Floyd Memorial Fund</a>.
+        </p>
+        <button aria-label="Close" style={{
+          border:"1px solid yellow",
+          boxShadow:"2px 2px 0px #A15FFB",
+          fontSize:"1.2rem",
+          padding:"0.5rem 1rem",
+          cursor:"pointer"
+        }} onClick={handleClose}><span aria-label="solidarity fist" role="img">✊🏿</span>No Justice, No Peace</button>
+      </div>
+    </div>
+  );
+};
 
 export const HomePageTemplate = ({
   content,
@@ -18,6 +104,7 @@ export const HomePageTemplate = ({
   team,
 }) => (
   <div className="ml5Grid__container--homePage">
+    <BLMSolidarityModal/>
     <section
       className="circles-1"
       style={{
