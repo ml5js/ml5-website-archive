65b6c194e9878ccfe630d944ade67baee4f04597
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-06-03 19:32:44 -0400

    Remove unused components

--- gatsby-node.js
+++ gatsby-node.js
@@ -45,30 +45,6 @@ exports.createPages = ({ actions, graphql }) => {
         }
       });
     });
-
-    // Tag pages:
-    let tags = [];
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach(edge => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags);
-      }
-    });
-    // Eliminate duplicate tags
-    tags = _.uniq(tags);
-
-    // Make tag pages
-    tags.forEach(tag => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`;
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags-page.js`),
-        context: {
-          tag
-        }
-      });
-    });
   });
 };
 
